cfaefa6ab08431abb91228fa034328f5766b8514
AhmadjonMurodullayev <murodullayevahmadjon26@gmail.com>
2024-09-08 13:48:59 +0500

    js

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,27 +1,83 @@
 import React, { useState, useEffect } from "react";
-import { Modal,ModalBody, ModalHeader, ModalFooter} from "react-bootstrap";
+import { Modal, ModalBody, ModalHeader, ModalFooter } from "reactstrap";
 import { nanoid } from "nanoid";
-const GlobalModal = (props) => {
-  const { open, toggle } = props;
-};
 
-return (
-  <Modal isOpen={open} toggle={toggle} className="modal-dialog-centered">
-    <ModalHeader>
-      <h2> Add car</h2>
-    </ModalHeader>
-    <ModalBody>
-      <div className="card">
-        <div className="card-body">
-          <form onSubmit={handleSubmit}>
-            <input type="text" name="name" placeholder="name" onChange={handleChange} className="form-control my-2" />
-          </form>
+const GlobalModal = ({ form, setForm, open, toggle, cars, setCars, update }) => {
+  useEffect(() => {
+    if (update) {
+      setForm(update);
+    }
+  }, [update, setForm]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (update.id) {
+      setCars(cars.map((car) =>
+        car.id === update.id ? { ...form, id: car.id } : car
+      ));
+    } else {
+      setCars([...cars, { ...form, id: nanoid() }]);
+    }
+    toggle(); // Modalni yopish
+  };
+
+  return (
+    <Modal isOpen={open} toggle={toggle} className="modal-dialog-centered">
+      <ModalHeader toggle={toggle}>
+        <h2>{update.id ? "Edit Car" : "Add Car"}</h2>
+      </ModalHeader>
+      <ModalBody>
+        <div className="card">
+          <div className="card-body">
+            <form onSubmit={handleSubmit} id="form">
+              <input
+                value={form.name || ""}
+                type="text"
+                name="name"
+                placeholder="Name"
+                onChange={handleChange}
+                className="form-control my-2"
+              />
+              <input
+                value={form.price || ""}
+                type="number"
+                name="price"
+                placeholder="Price"
+                onChange={handleChange}
+                className="form-control my-2"
+              />
+              <input
+                value={form.year || ""}
+                type="number"
+                name="year"
+                placeholder="Year"
+                onChange={handleChange}
+                className="form-control my-2"
+              />
+              <input
+                value={form.color || ""}
+                type="text"
+                name="color"
+                placeholder="Color"
+                onChange={handleChange}
+                className="form-control my-2"
+              />
+            </form>
+          </div>
         </div>
-      </div>
-    </ModalBody>
-    <ModalFooter>
-    </ModalFooter>
-  </Modal>
-);
+      </ModalBody>
+      <ModalFooter>
+        <button className="btn btn-success" form="form" type="submit">
+          Save
+        </button>
+      </ModalFooter>
+    </Modal>
+  );
+};
 
 export default GlobalModal;
--- src/pages/cars/index.jsx
+++ src/pages/cars/index.jsx
@@ -1,21 +1,104 @@
+// import React, { useState } from "react";
+// import { NavLink } from "react-router-dom";
+// import  GlobalModal from '../../components/modal'
+ 
+// function Index() {
+//   const [open, setOpen] = useState(false)
+//   const [update, setUpdet] = useState({})
+//   const [cars, setCars] = useState([
+//     { id: 1, name: "ferari", price: 100, year: 2024, color: "bulur" },
+//     { id: 1, name: "ferari", price: 100, year: 2024, color: "bulur" },
+//     { id: 1, name: "ferari", price: 100, year: 2024, color: "bulur" }
+//   ]);
+  
+//   const editModal =(item)=>{
+//     setUpdet(item)
+//     setOpen(true)
+
+//   }
+//   const toggle =()=>{
+//     setOpen(false)
+//     setUpdet({})
+
+//   }
+//   return (
+//     <div className="container">
+//       <GlobalModal open={open} toggle={toggle} cars={cars} setCars={setCars} update={update} />
+//       <div className="row">
+//         <div className="col-md-3 my-4">
+//           <button className="btn btn-primary" onClick={()=> setOpen(true)}>Open modal</button>
+//         </div>
+//       </div>
+//       <div className="row">
+//         <div className="col-md-8">
+//           <table className="table table-bordered">
+//             <thead>
+//               <tr>
+//                 <th>T/r</th>
+//                 <th>Name</th>
+//                 <th>Price</th>
+//                 <th>Year</th>
+//                 <th>Color</th>
+//                 <th>Action</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {cars.map((item, index) => {
+//                 return (
+//                   <tr key={index}>
+//                     <td>{index + 1}</td>
+//                     <td>{item.name}</td>
+//                     <td>{item.price}</td>
+//                     <td>{item.year}</td>
+//                     <td>{item.color}</td>
+//                     <td>
+//                       <NavLink to={`/cars${item.id} `} className="btn btn-info">
+//                       view
+//                       </NavLink>
+//                       <button className="btn btn-waring" onClick={(item)=>editModal}>edit</button>
+//                     </td>
+//                   </tr>
+//                 );
+//               })}
+//             </tbody>
+//           </table>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Index;
+
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import  GlobalModal from '../../components/modal'
- 
+import GlobalModal from '../../components/modal';
+
 function Index() {
-  const [open, setOpen] = useState()
+  const [open, setOpen] = useState(false);
+  const [form, setForm] = useState({});
   const [cars, setCars] = useState([
-    { id: 1, name: "ferari", price: 100, year: 2024, color: "bulur" },
-    { id: 1, name: "ferari", price: 100, year: 2024, color: "bulur" },
-    { id: 1, name: "ferari", price: 100, year: 2024, color: "bulur" }
+    { id: 1, name: "Ferrari", price: 100, year: 2024, color: "Blue" },
+    { id: 2, name: "Tesla", price: 200, year: 2024, color: "Red" },
+    { id: 3, name: "BMW", price: 300, year: 2024, color: "Black" }
   ]);
+
+  const editModal = (item) => {
+    setForm(item);
+    setOpen(true);
+  };
+
+  const toggle = () => {
+    setOpen(false);
+    setForm({});
+  };
 
   return (
     <div className="container">
-      <GlobalModal open={open} toggle={()=> setOpen(false)} />
+      <GlobalModal open={open} toggle={toggle} form={form} setForm={setForm} cars={cars} setCars={setCars} update={form} />
       <div className="row">
         <div className="col-md-3 my-4">
-          <button className="btn btn-primary" onClick={()=> setOpen(true)}>Open modal</button>
+          <button className="btn btn-primary" onClick={() => setOpen(true)}>Open Modal</button>
         </div>
       </div>
       <div className="row">
@@ -32,23 +115,21 @@ function Index() {
               </tr>
             </thead>
             <tbody>
-              {cars.map((item, index) => {
-                return (
-                  <tr key={index}>
-                    <td>{index + 1}</td>
-                    <td>{item.name}</td>
-                    <td>{item.price}</td>
-                    <td>{item.year}</td>
-                    <td>{item.color}</td>
-                    <td>
-                      <NavLink to={`/cars${item.id} `} className="btn btn-info">
-
-                      view
-                      </NavLink>
-                    </td>
-                  </tr>
-                );
-              })}
+              {cars.map((item, index) => (
+                <tr key={item.id}>
+                  <td>{index + 1}</td>
+                  <td>{item.name}</td>
+                  <td>{item.price}</td>
+                  <td>{item.year}</td>
+                  <td>{item.color}</td>
+                  <td>
+                    <NavLink to={`/cars/${item.id}`} className="btn btn-info">
+                      View
+                    </NavLink>
+                    <button className="btn btn-warning" onClick={() => editModal(item)}>Edit</button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
